0f9f066018a21c6fd053acc371311f7be3d0a2d9
rajark <rajark@paypal.com>
2025-11-21 17:33:17 +0530

    FEAT: Starting server from tests.

--- src/test/java/com/raja/AppTest.java
+++ src/test/java/com/raja/AppTest.java
@@ -3,9 +3,11 @@ package com.raja;
 
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.options.UiAutomator2Options;
+import io.appium.java_client.service.local.AppiumDriverLocalService;
+import io.appium.java_client.service.local.AppiumServiceBuilder;
 import org.openqa.selenium.Capabilities;
 import org.testng.annotations.Test;
-
+import java.io.File;
 import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URISyntaxException;
@@ -22,12 +24,26 @@ public class AppTest {
     @Test
     public void shouldAnswerWithTrue() throws MalformedURLException, URISyntaxException
     {
+        // Programmatically start Appium server
+        AppiumDriverLocalService service = new AppiumServiceBuilder()
+                .withAppiumJS(new File("/usr/local/lib/node_modules/appium/build/lib/main.js"))
+                .withIPAddress("127.0.0.1")
+                .usingPort(4723)
+                .build();
+        service.start();
+
         UiAutomator2Options options = new UiAutomator2Options();
         options.setDeviceName("Medium Phone API 36 (2)");
         options.setApp("/Users/rajark/LeisureWork/MyAppium/src/test/resources/ApiDemos-debug.apk");
 
         URL url =  new URI("http://127.0.0.1:4723").toURL();
         AndroidDriver androidDriver = new AndroidDriver(url, options);
+
+        // Actual automation
+
+
         androidDriver.quit();
+        service.stop();
+
     }
 }
