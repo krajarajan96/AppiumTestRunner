90a6442aa45f9dfed94ff1f1d3a4e2bdaabdf205
rajark <rajark@paypal.com>
2025-11-24 19:41:51 +0530

    FEAT: Added long press gesture test.

--- src/test/java/com/raja/AppiumTests.kt
+++ src/test/java/com/raja/AppiumTests.kt
@@ -1,7 +1,10 @@
 package com.raja
 
+import com.google.common.collect.ImmutableMap
 import io.appium.java_client.AppiumBy
 import org.openqa.selenium.By
+import org.openqa.selenium.JavascriptExecutor
+import org.openqa.selenium.remote.RemoteWebElement
 import org.testng.Assert
 import org.testng.annotations.Test
 import java.net.MalformedURLException
@@ -29,5 +32,22 @@ class AppiumTests : BaseTest() {
 
         driver.findElement(AppiumBy.id("android:id/edit")).sendKeys("RajaWifi")
         driver.findElements(AppiumBy.className("android.widget.Button"))[1].click()
+    }
+
+    @Test
+    fun longPressGestureTest() {
+        driver.findElement(AppiumBy.accessibilityId("Views")).click()
+        driver.findElement(AppiumBy.xpath("//android.widget.TextView[@content-desc='Expandable Lists']")).click()
+        driver.findElement(AppiumBy.accessibilityId("1. Custom Adapter")).click()
+
+        val peopleNamesElement = driver.findElement(AppiumBy.xpath("//android.widget.TextView[@text='People Names']"))
+        (driver as JavascriptExecutor).executeScript(
+            "mobile: longClickGesture",
+            ImmutableMap.of(
+                "elementId", (peopleNamesElement as RemoteWebElement).id,
+                "duration", 2000
+            )
+        )
+        Thread.sleep(2000)
     }
 }
