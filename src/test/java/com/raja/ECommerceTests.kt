9244943fef02e8e1736624dd44ccda89ff1dbe04
rajark <rajark@paypal.com>
2025-11-28 12:02:32 +0530

    FEAT: Added ecommerce app tests - cart tests.

--- src/test/java/com/raja/ECommerceTests.kt
+++ src/test/java/com/raja/ECommerceTests.kt
@@ -20,12 +20,11 @@ class ECommerceTests: BaseTest() {
         driver.findElement(AppiumBy.id("com.androidsample.generalstore:id/spinnerCountry"))
             .click()
 
-        scrollToElement("Chad")
-        driver.findElement(AppiumBy.xpath("//android.widget.TextView[@text=\"Chad\"]"))
+        scrollToElement("Argentina")
+        driver.findElement(AppiumBy.xpath("//android.widget.TextView[@text=\"Argentina\"]"))
             .click()
         driver.findElement(AppiumBy.id("com.androidsample.generalstore:id/btnLetsShop"))
             .click()
-        Thread.sleep(3000)
     }
 
     @Test
@@ -41,5 +40,87 @@ class ECommerceTests: BaseTest() {
         Assert.assertTrue(toast.isDisplayed)
         val toastMsg = toast.getAttribute("name")
         Assert.assertEquals(toastMsg, "Please enter your name")
+    }
+
+    @Test
+    fun testAddToCart() {
+        driver.findElement(AppiumBy.id("com.androidsample.generalstore:id/nameField")).click()
+        driver.findElement(AppiumBy.id("com.androidsample.generalstore:id/nameField"))
+            .sendKeys("Rajarajan K")
+        driver.findElement(AppiumBy.id("com.androidsample.generalstore:id/radioFemale"))
+            .click()
+        driver.findElement(AppiumBy.id("com.androidsample.generalstore:id/spinnerCountry"))
+            .click()
+
+        scrollToElement("Argentina")
+        driver.findElement(AppiumBy.xpath("//android.widget.TextView[@text=\"Argentina\"]"))
+            .click()
+        driver.findElement(AppiumBy.id("com.androidsample.generalstore:id/btnLetsShop"))
+            .click()
+
+        scrollToElement("Jordan 6 Rings")
+        val elements = driver.findElements(AppiumBy.id("com.androidsample.generalstore:id/productName"))
+        for (i in 0 until elements.size) {
+            if (elements[i].getAttribute("text") == "Jordan 6 Rings") {
+                val addToCart = driver.findElement(AppiumBy.id("com.androidsample.generalstore:id/productAddCart"))
+                addToCart.click()
+                Assert.assertEquals(
+                    addToCart.text,
+                    "ADDED TO CART"
+                )
+                break
+            }
+        }
+
+        val cartBtn = driver.findElement(AppiumBy.id("com.androidsample.generalstore:id/appbar_btn_cart"))
+        cartBtn.click()
+        val wait = WebDriverWait(driver, Duration.ofSeconds(2))
+        wait.until(
+            ExpectedConditions.attributeContains(
+                driver.findElement(AppiumBy.id("com.androidsample.generalstore:id/toolbar_title")),
+                "text",
+                "Cart"
+            )
+        )
+
+        val element = driver.findElement(AppiumBy.id("com.androidsample.generalstore:id/productName"))
+        Assert.assertEquals(element.text, "Jordan 6 Rings")
+    }
+
+    @Test
+    fun testCartTotal() {
+        driver.findElement(AppiumBy.id("com.androidsample.generalstore:id/nameField")).click()
+        driver.findElement(AppiumBy.id("com.androidsample.generalstore:id/nameField"))
+            .sendKeys("Rajarajan K")
+        driver.findElement(AppiumBy.id("com.androidsample.generalstore:id/radioFemale"))
+            .click()
+        driver.findElement(AppiumBy.id("com.androidsample.generalstore:id/spinnerCountry"))
+            .click()
+
+        scrollToElement("Argentina")
+        driver.findElement(AppiumBy.xpath("//android.widget.TextView[@text=\"Argentina\"]"))
+            .click()
+        driver.findElement(AppiumBy.id("com.androidsample.generalstore:id/btnLetsShop"))
+            .click()
+
+
+        val addToCarts = driver.findElements(AppiumBy.id("com.androidsample.generalstore:id/productAddCart"))
+        addToCarts[0].click()
+        addToCarts[1].click()
+
+        val cartBtn = driver.findElement(AppiumBy.id("com.androidsample.generalstore:id/appbar_btn_cart"))
+        cartBtn.click()
+
+        val wait = WebDriverWait(driver, Duration.ofSeconds(2))
+        wait.until(
+            ExpectedConditions.attributeContains(
+                driver.findElement(AppiumBy.id("com.androidsample.generalstore:id/toolbar_title")),
+                "text",
+                "Cart"
+            )
+        )
+
+        val amount = driver.findElement(AppiumBy.id("com.androidsample.generalstore:id/totalAmountLbl"))
+        Assert.assertEquals(amount.text, "\$ 280.97")
     }
 }
